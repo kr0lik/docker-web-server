9825d7cd82eef28622105e9fb0c752e119f92da2
denis <denis@denis>
2019-03-18 12:37:34 +0300

    adding go

--- go/test/main.go
+++ go/test/main.go
@@ -1,19 +1,19 @@
 package main 
 
 import (
-	"fmt"
-	"os"
-	"net/http"
+    "fmt"
+    "os"
+    "net/http"
 )
 
 func main () { 
-    fmt.Println ("Starting server...");
-	
-	var PORT string
-	PORT = os.Getenv("PORT")
-	
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World from path: %s\n", r.URL.Path)
-	})
-	http.ListenAndServe(":" + PORT, nil)
+    fmt.Println ("Starting test server...");
+
+    var PORT string
+    PORT = os.Getenv("PORT")
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello %s from path: %s\n", r.UserAgent(), r.URL.Path)
+    })
+    http.ListenAndServe(":" + PORT, nil)
 }
